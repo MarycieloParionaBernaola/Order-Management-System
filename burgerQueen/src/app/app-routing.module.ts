244d7b33761ef576a6fd881f0ba3e34be60f0a97
marycielopb <mparionabernaola@gmail.com>
2020-12-27 18:48:26 -0500

    waiter-chef routing implemented - closes #6

--- burgerQueen/src/app/app-routing.module.ts
+++ burgerQueen/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WaiterComponent } from './components/waiter/waiter.component';
+import { ChefComponent } from './components/chef/chef.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'waiter', component: WaiterComponent },
+  { path: 'chef', component: ChefComponent },
+  { path: '', component: WaiterComponent , pathMatch: 'full'}, // Home
+  { path: '**', redirectTo: '/', pathMatch: 'full' } // 404 not found
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
