9fdfd4208b71b9045d52a3961f134e14fe63ca25
marycielopb <mparionabernaola@gmail.com>
2021-01-14 19:48:31 -0500

    deployed app in firebase hosting - closes #25

--- burgerQueen/src/environments/environment.prod.ts
+++ burgerQueen/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC2PdyyoY54QQJIUGykl346taYlmvo9M6w",
+    authDomain: "sm-restaurant.firebaseapp.com",
+    projectId: "sm-restaurant",
+    storageBucket: "sm-restaurant.appspot.com",
+    messagingSenderId: "665781065499",
+    appId: "1:665781065499:web:3d4236ba9a9718a88ca5be",
+    measurementId: "G-T9L0BJ0R6Z"
+  }
 };
